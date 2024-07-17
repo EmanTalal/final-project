3d5d9e9819dd8574e1f0cae8002e906f83525fc1
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-07-17 14:50:10 +0300

    added a button in App.jsx to navigate to plan page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <h1>Home</h1>
+      <h1>hhhh</h1>
+      <Link to={'Plan'}>
+        {' '}
+        <button className="btn ml-8">Plan page</button>
+      </Link>
     </>
   );
 }
